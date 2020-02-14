9b4469c84c65a06df4b3784a04f924a1c4fb4402
chocopie116 <chocopie116@gmail.com>
2020-02-14 15:53:52 +0900

    リソースからルーティングを考えてみた

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,18 +1,105 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/**
+ * ユーザー情報
+ */
+Route::post('/users', function () {
+    return response()->json([
+        'id' => 1
+    ], 201);
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/users/me', function () {
+    return response()->json([
+        'name' => '田中太郎'
+    ], 200);
+});
+
+Route::put('/users/me', function () {
+    return response()->json([
+        'name' => '田中太郎'
+    ]);
+});
+
+Route::get('/users/{id}', function () {
+    return response()->json([
+        'name' => '田中太郎'
+    ]);
+});
+
+
+
+/**
+ * プロフィール情報
+ */
+Route::post('/users/profiles', function () {
+    return response()->json([
+        'id' => 1
+    ], 201);
+});
+
+Route::put('/users/me/profiles', function () {
+    return response()->json([
+        'id' => 1
+    ]);
+});
+
+Route::get('/users/me/profiles', function () {
+    return response()->json([
+        'id' => 1
+    ]);
+});
+
+Route::get('/users/profiles', function () {
+    return response()->json([
+        'id' => 1
+    ]);
+});
+//他人のプロフィールをみる
+Route::get('/users/{id}/profiles', function () {
+    return response()->json([
+        'id' => 1
+    ]);
+});
+
+/**
+ * LIKE情報
+ */
+Route::post('/users/{id}/likes', function () {
+    return response()->json([
+        'id' => 1
+    ], 201);
+});
+
+//他人のLIKEをみる
+Route::get('/users/me/likes', function () {
+    return response()->json([
+        'id' => 1
+    ]);
+});
+
+/**
+ * マッチング情報
+ */
+Route::get('/users/me/matchings', function () {
+    return response()->json([
+        'id' => 1
+    ], 201);
+});
+
+/**
+ * メッセージ情報
+ */
+Route::post('/matchings/{id}/messages', function () {
+    return response()->json([
+        'id' => 1
+    ], 201);
+});
+
+Route::get('/matchings/{id}/messages', function () {
+    return response()->json([
+        'id' => 1
+    ]);
 });
